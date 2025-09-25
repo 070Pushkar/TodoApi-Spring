044cad9027fb329723f3e9bb90a2f2b7c9059378
070Pushkar <pushkarsrivastav456@gmail.com>
2025-09-25 17:29:21 +0530

    create new Todo

--- src/main/java/com/example/TodoApiSpring/TodoController.java
+++ src/main/java/com/example/TodoApiSpring/TodoController.java
@@ -2,6 +2,8 @@ package com.example.TodoApiSpring;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,5 +23,11 @@ public TodoController(){
 @GetMapping("/todos")
     public List<Todo> getTodos(){
     return todoList;
+}
+
+  @PostMapping("/todos")
+  public Todo createTodo(@RequestBody Todo newTodo){
+    todoList.add(newTodo);
+    return newTodo;
 }
 }
